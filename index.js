0b94b1264cb1303272978bfd0c3ad2cfb07cc136
Cosdaman <Cosdaguy@gmail.com>
2021-11-30 21:01:44 -0800

    finished main menu

--- index.js
+++ index.js
@@ -5,6 +5,66 @@ function mainMenuInq() {
     inquirer.prompt(questionBank.mainMenu)
         .then(
             (response) => {
+                switch (response.mainMenu) {
+                    case 'View':
+                        viewMenuInq();
+                        break;
+
+                    case 'Add':
+                        addMenuInq();
+                        break;
+
+                    case 'Update':
+                        updateMenuInq();
+                        break;
+                }
+            })
+        .catch(
+            (error) => {
+                if (error.isTtyError) {
+                    console.log("error", error)
+                } else {
+                    console.log("something else went wrong", error)
+                }
+            })
+}
+
+function viewMenuInq() {
+    inquirer.prompt(questionBank.viewMenu)
+        .then(
+            (response) => {
+                console.log(response)
+            })
+        .catch(
+            (error) => {
+                if (error.isTtyError) {
+                    console.log("error", error)
+                } else {
+                    console.log("something else went wrong", error)
+                }
+            })
+}
+
+function addMenuInq() {
+    inquirer.prompt(questionBank.addMenu)
+        .then(
+            (response) => {
+                console.log(response)
+            })
+        .catch(
+            (error) => {
+                if (error.isTtyError) {
+                    console.log("error", error)
+                } else {
+                    console.log("something else went wrong", error)
+                }
+            })
+}
+
+function updateMenuInq() {
+    inquirer.prompt(questionBank.updateMenu)
+        .then(
+            (response) => {
                 console.log(response)
             })
         .catch(
--- src/questionBank.js
+++ src/questionBank.js
@@ -1,30 +1,30 @@
-//main menu
-const mainMenu = [
-    {
-        type: "list",
-        message: "Select an option from the choices below: ",
-        name: 'mainMenu',
-        choices: ["View", "Add", "Update"]
-    }
-]
+//menus
+const mainMenu = [{
+    type: "list",
+    message: "Select an option from the choices below: ",
+    name: 'mainMenu',
+    choices: ["View", "Add", "Update"]
+}]
 
-const viewMenu = [
-    {
-        type: "list",
-        message: "Select an option from the choices below: ",
-        name: 'viewMenu',
-        choices: ["View All Departments", "View All Roles", "View All Employees"]
-    }
-]
+const viewMenu = [{
+    type: "list",
+    message: "Select an option from the choices below: ",
+    name: 'viewMenu',
+    choices: ["View All Departments", "View All Roles", "View All Employees"]
+}]
 
-const addMenu = [
-    {
-        type: "list",
-        message: "Select an option from the choices below: ",
-        name: 'addMenu',
-        choices: ["Add A Department", "Add A Role", "Add an Employee"]
-    }
-]
+const addMenu = [{
+    type: "list",
+    message: "Select an option from the choices below: ",
+    name: 'addMenu',
+    choices: ["Add A Department", "Add A Role", "Add an Employee"]
+}]
 
-module.exports =  {mainMenu,viewMenu,addMenu}
-//, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+const updateMenu = [{
+    type: "list",
+    message: "Select an option from the choices below: ",
+    name: 'addMenu',
+    choices: ["Update an Employee"]
+}]
+
+module.exports = { mainMenu, viewMenu, addMenu, updateMenu }
