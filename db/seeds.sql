792240c1e0246fd42abe160da210a37480bb2642
Cosdaman <Cosdaguy@gmail.com>
2021-12-04 10:26:27 -0800

    seeds completed

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,3 +14,20 @@ VALUES
     ("Safety Officer", 20000, 2),
     ("Javascript Developer", 40000, 3),
     ("Full Stack Developer", 50000, 3);
+
+insert into
+    employees (first_name, last_name, role_id)
+values
+    ('Thedric', 'Brothwood', 1),
+    ('Russell', 'McManamen', 2),
+    ('Kiele', 'Smedmore', 3),
+    ('Jodi', 'Cadogan', 4),
+    ('Tani', 'Chislett', 5);
+
+insert into
+    employees (first_name, last_name, role_id, manager_id)
+values
+    ('Cassie', 'Lukas', 1, 1),
+    ('Dulcea', 'Piscopo', 2, 2),
+    ('Wallie', 'Pancast', 3, 3),
+    ('Marissa', 'Peasegod', 4, 4);
